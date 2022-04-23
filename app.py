255b33053d7add38d59870f8768053ca89ae4820
rohitwtbs <12rohit4@gmail.com>
2022-04-23 07:04:46 -0400

    moving ahjead with text scanning

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+"""
 from tabula import read_pdf
-df = read_pdf('credit_bill.pdf',pages='all')
+df = read_pdf('bill.pdf',pages='all')
 import pdb
 pdb.set_trace()
 print(df)
+"""
+import camelot.io as camelot
+pdf = camelot.read_pdf('bill.pdf',pages='all')
+print(pdf)
+dataframe = pdf[0].df
+print(dataframe)
+import pdb
+pdb.set_trace()
